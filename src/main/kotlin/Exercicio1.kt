c10dda4c843ce7a152036300613ca787ba2dfc42
Ana Paula Pasini <anapaulapasini@hotmail.com>
2023-07-24 17:59:13 -0300

    init

---
+++ src/main/kotlin/Exercicio1.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+//Crie um novo arquivo com uma função main
+
+    //Declare uma variável mutável capaz de armazenar seu nome completo
+    var nomeprimeiro = "Ana Paula Pasini Ghisi"
+    println(nomeprimeiro)
+
+    //Declare uma variável de texto sem valor algum.
+    var nomeVazio: String
+
+    //Declare uma variável imutável com o menor tipo de dado possível capaz de armazenar o número que você
+    //calça.
+    val calcado: Byte
+    println("Meu numero e 36")
+
+    //Declare uma variável capaz de armazenar o PIB do Brasil
+    var pibBrasil = "1.869.000.000.000"
+    println(pibBrasil)
+
+    //Declare uma variável capaz de armazenar a população do Brasil
+    var populacaoBrasil = "211.000.000"
+    println(populacaoBrasil)
+
+
+}
